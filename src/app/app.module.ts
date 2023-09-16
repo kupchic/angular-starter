f5e155d304cac575c76e0235dabb9db8ed19e609
Akupchenia <akupchenia@elinext.com>
2023-09-17 00:12:10 +0300

    1angular-v16.2.2-SSR basic configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { APP_ID, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideClientHydration } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [AppRoutingModule],
+  providers: [{ provide: APP_ID, useValue: 'serverApp' }, provideClientHydration()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
