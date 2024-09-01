4eb1d0dfe8df217ea0bd76e7b2dcd9a893a55af5
akupchenia <kup4enya1999@gmail.com>
2024-09-01 12:36:26 +0300

    angular 17 ssr settings completed

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,7 @@ import '@angular/platform-server/init';
 
 import { enableProdMode } from '@angular/core';
 import { environment } from '@env';
+import { AppServerModule } from './app/app.server.module';
 
 if (environment.production) {
   enableProdMode();
@@ -9,3 +10,7 @@ if (environment.production) {
 
 export { AppServerModule } from './app/app.server.module';
 export { renderModule } from '@angular/platform-server';
+
+const bootstrap = AppServerModule;
+
+export default bootstrap;
